0b60f56680717e844b6ae392adab446a5621e007
sswander <leonardosyahputra@yahoo.com>
2016-06-26 00:51:06 -0700

    added hardcoded participants and title
    Signed-off-by: sswander <leonardosyahputra@yahoo.com>

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 var socket = io.connect('http://localhost:5000/branch');
 var chatHistory = "";
 var username = "";
+var participants = [["Hayley", "Jack", "Jessica", "Jill", "You"], ["Brad", "Beth", "Margaret", "Steve", "Tyrone", "You"], ["Giselle", "Mams", "Pops", "You"]];
+var conversationTitles = ["Friends 5ever", "Bros 5lyf", "Family"];
 
 $(document).ready(function () {
 
@@ -23,10 +25,8 @@ $(document).ready(function () {
 		$(this).addClass('conversation-item-selected');
 		$('.chat-title-bar-title').text($(this).find('.conversation-title').text());
 		$('#conversation-title-avatar').attr("src",$(this).find('.conversation-avatar').prop('src'));
+		updateParticipants($(this).index());
 	});
-
-	// on startup function calls
-	showFirstConversation();
 });
 
 function populateChat(chatHistory) {
@@ -34,6 +34,10 @@ function populateChat(chatHistory) {
 		for (var topic in chatHistory) {
 			if (chatHistory.hasOwnProperty(topic)) {
 				console.log(chatHistory[topic]);
+				var currentMessages = chatHistory[topic]["messages"];
+				console.log(currentMessages);
+				var lastMesage = currentMessages[currentMessages.length - 1];
+				showFirstConversation(lastMesage.username + ": " + lastMesage.message);
 			}
 		}
 	}
@@ -60,11 +64,26 @@ function sendButtonOnClickListener() {
 }
 
 
-function showFirstConversation() {
+function showFirstConversation(lastMessage) {
 	var firstConversation = $('ul.conversations li:nth-child(1)');
 	firstConversation.addClass('conversation-item-selected');
 	$('.chat-title-bar-title').text(firstConversation.find('.conversation-title').text());
 	$('#conversation-title-avatar').attr("src",firstConversation.find('.conversation-avatar').prop('src'));
+	updateLastMessage(firstConversation, lastMessage);
+	updateParticipants(0);
+}
+
+function updateParticipants(conversationIndex) {
+	var participantString = "";
+	for (var i = 0; i < participants[conversationIndex].length; i++) {
+		participantString += participants[conversationIndex][i] + ", ";
+	};
+	participantString = participantString.substring(0, participantString.length - 2);
+	$('.chat-title-bar-participants').html(participantString);
+}
+
+function updateLastMessage(conversation, lastMessage) {
+	conversation.find('.conversation-last-message').html(lastMessage);
 }
 
 function sendChat(){
